edb15f1e582af2a70770958ef28cac46dcf0db38
Sonberg <per.sonberg@gmail.com>
2016-03-30 14:14:07 +0200

    view setup

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-
 <html>
 
 <head>
@@ -10,13 +9,9 @@
 </head>
 
 <body>
-    <div class="container">
-        <div class="content">
-            <div class="title">Laravel 5</div>
-        </div>
-    </div>
+    @yield('content')
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
 </body>
- 
+
 </html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('app') @section('content')
+<div class="container">
+    <div class="content">
+        <h1 class="title">Red Cross</h1>
+    </div>
+</div>
+@endsection
