1be3c327530f89a3d50706b7e8716b310d207653
”Simon <simonturesson@gmail.com>
2016-04-11 16:47:58 +0200

    Dashboard landing grid is now fixed, added static statistics(numbers)

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,43 @@
 @extends('app') @section('content') @include('dashboard.navbar')
 
 <div class="ui page grid main" id="dashboard">
-    <div class="row center-blocks">
+
+    <div class="ui statistics center">
+        <div class="statistic">
+            <div class="value">
+                22
+            </div>
+            <div class="label">
+                Faves
+            </div>
+        </div>
+        <div class="statistic">
+            <div class="value">
+                31,200
+            </div>
+            <div class="label">
+                Views
+            </div>
+        </div>
+        <div class="statistic">
+            <div class="value">
+                22
+            </div>
+            <div class="label">
+                Members
+            </div>
+        </div>
+        <div class="statistic">
+            <div class="value">
+                22
+            </div>
+            <div class="label">
+                Members
+            </div>
+        </div>
+    </div>
+
+    <div class="row center">
 
 
        {{--   <div class="ui link cards">
