4fc9115b0409eb74b21a4304aaa9c2b1d200fc7e
”Simon <simonturesson@gmail.com>
2016-04-12 08:45:27 +0200

    dashboard css fixes

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -66,140 +66,49 @@
                 </div>
                 <!-- @endforeach -->--}}
 
-<div class="col-xs-12 col-sm-12 col-md-6 paddingcols">
-    <div class="column padding-reset">
-                            <div class="card">
-                                <div class="content">
-                                    <div class="description">
+        <div class="col-xs-12 col-sm-12 col-md-6 paddingcols">
+            <div class="column padding-reset">
+                <div class="card">
+                    <div class="content">
+                        <div class="description">
 
-                                        <div class="ui horizontal segments">
-                                            <div class="ui segment"
-                                                 <p>Namn Namnsson</p>
-                                            </div>
-                                        <div class="ui segment">
-                                        <p>Kon</p>
-                                    </div>
-                                    <div class="ui segment">
-                                    <p>Land</p>
-                                </div>
+                            <div class="ui horizontal segments">
                                 <div class="ui segment">
+                                <p>Namn Namnsson</p>
+                            </div>
+                            <div class="ui segment">
+                                <p>Kon</p>
+                            </div>
+                            <div class="ui segment">
+                                <p>Land</p>
+                            </div>
+                            <div class="ui segment">
                                 <p>dagar</p>
-                            </div></div>
-                                        </div>
-
-
-                                    </div>
-
-                                <div class="ui buttons bottom attached button">
-                                    <button class="ui button matchbtn">Matcha nu(xx)</button>
-                                    <button class="ui button">Mer info</button>
-                                </div>
                             </div>
-                            </div>
-
-
-
-    </div>
-    <div class="col-xs-12 col-sm-12 col-md-6 paddingcols">
-        <div class="column padding-reset">
-            <div class="card">
-                <div class="content">
-                    <div class="description">
-
-                        <div class="ui horizontal segments">
-                            <div class="ui segment"
-                            <p>Namn Namnsson</p>
                         </div>
-                        <div class="ui segment">
-                            <p>Kon</p>
-                        </div>
-                        <div class="ui segment">
-                            <p>Land</p>
-                        </div>
-                        <div class="ui segment">
-                            <p>dagar</p>
-                        </div></div>
+                    </div>
+
+
                 </div>
 
-
-            </div>
-
-            <div class="ui buttons bottom attached button">
-                <button class="ui button matchbtn">Matcha nu(xx)</button>
-                <button class="ui button">Mer info</button>
+                <div class="ui buttons bottom attached button">
+                    <button class="ui button matchbtn">Matcha nu(xx)</button>
+                    <button class="ui button">Mer info</button>
+                </div>
             </div>
         </div>
+
+
     </div>
 
-
-
 </div>
 
-<div class="col-xs-12 col-sm-12 col-md-6 paddingcols">
-    <div class="column padding-reset">
-        <div class="card">
-            <div class="content">
-                <div class="description">
 
-                    <div class="ui horizontal segments">
-                        <div class="ui segment"
-                        <p>Namn Namnsson</p>
-                    </div>
-                    <div class="ui segment">
-                        <p>Kon</p>
-                    </div>
-                    <div class="ui segment">
-                        <p>Land</p>
-                    </div>
-                    <div class="ui segment">
-                        <p>dagar</p>
-                    </div></div>
-            </div>
-
-
-        </div>
-
-        <div class="ui buttons bottom attached button">
-            <button class="ui button matchbtn">Matcha nu(xx)</button>
-            <button class="ui button">Mer info</button>
-        </div>
-    </div>
-</div>
 
 
 
 </div>
 
-<div class="col-xs-12 col-sm-12 col-md-6 paddingcols">
-    <div class="column padding-reset">
-        <div class="card">
-            <div class="content">
-                <div class="description">
-
-                    <div class="ui horizontal segments">
-                        <div class="ui segment"
-                        <p>Namn Namnsson</p>
-                    </div>
-                    <div class="ui segment">
-                        <p>Kon</p>
-                    </div>
-                    <div class="ui segment">
-                        <p>Land</p>
-                    </div>
-                    <div class="ui segment">
-                        <p>dagar</p>
-                    </div></div>
-            </div>
-
-
-        </div>
-
-        <div class="ui buttons bottom attached button">
-            <button class="ui button matchbtn">Matcha nu(xx)</button>
-            <button class="ui button">Mer info</button>
-        </div>
-    </div>
-</div>
 
 
 
