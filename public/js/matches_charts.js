81fa49d38af39472b3d8ed38bc326cfef88e0ea3
”Simon <simonturesson@gmail.com>
2016-04-19 13:08:17 +0200

    statistics

--- public/js/matches_charts.js
+++ public/js/matches_charts.js
@@ -1,4 +1,4 @@
-var ctx = document.getElementById("c").getContext("2d");
+var ctx = document.getElementById("matches").getContext("2d");
 var data = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [{
@@ -22,3 +22,56 @@ var data = {
     }]
 };
 var MyNewChart = new Chart(ctx).Line(data);
+
+
+var newimmigrants = document.getElementById("newimmigrants").getContext("2d");
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+        label: "My First dataset",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [65, 59, 80, 81, 56, 55, 40]
+    }, {
+        label: "My Second dataset",
+        fillColor: "rgba(151,187,205,0.2)",
+        strokeColor: "rgba(151,187,205,1)",
+        pointColor: "rgba(151,187,205,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+        data: [28, 48, 40, 19, 86, 27, 90]
+    }]
+};
+var MyNewChart = new Chart(newimmigrants).Line(data);
+
+
+var newfriends = document.getElementById("newfriends").getContext("2d");
+var data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+        label: "My First dataset",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [65, 59, 80, 81, 56, 55, 40]
+    }, {
+        label: "My Second dataset",
+        fillColor: "rgba(151,187,205,0.2)",
+        strokeColor: "rgba(151,187,205,1)",
+        pointColor: "rgba(151,187,205,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+        data: [28, 48, 40, 19, 86, 27, 90]
+    }]
+};
+var MyNewChart = new Chart(newfriends).Line(data);
+
