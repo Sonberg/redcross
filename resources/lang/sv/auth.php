d3ffb2292f714aa710c89a804bdabae807044929
”Simon <simonturesson@gmail.com>
2016-04-22 07:21:02 +0200

    swedish translation

--- resources/lang/sv/auth.php
+++ resources/lang/sv/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Ditt lösenord eller användarnamn är fel.',
+    'throttle' => 'För många login försök. Testa igen senare..',
 
 ];
