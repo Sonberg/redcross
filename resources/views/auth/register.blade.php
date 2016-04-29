f3009a4bdc95de481a0159ef4c46305f14cb47e3
Sonberg <per.sonberg@gmail.com>
2016-04-29 10:48:08 +0200

    register html update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,13 +5,13 @@
     <div class="column six wide form-holder">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <h2 class="center aligned header form-head">Register</h2>
+                <h2 class="center aligned header form-head margin-bottom-large">Register</h2>
                 <div class="panel-body">
                     <form class="form ui" role="form" method="POST" action="{{ url('/register') }}">
                         {!! csrf_field() !!}
 
-                        <div class="field {{ $errors->has('name') ? ' has-error' : '' }}">
-                            <div class="col-md-6">
+                        <div class="{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <div class="margin-bottom-large">
                                 <input type="text" class="form-control" name="name" placeholder="Name" value="{{ old('name') }}">
 
                                 @if ($errors->has('name'))
@@ -21,9 +21,9 @@
                                 @endif
                             </div>
                         </div>
-                        
-                        <div class="field {{ $errors->has('title') ? ' has-error' : '' }}">
-                            <div class="col-md-6">
+
+                        <div class="{{ $errors->has('title') ? ' has-error' : '' }}">
+                            <div class="margin-bottom-large">
                                 <input type="text" class="form-control" name="title" placeholder="Title" value="{{ old('title') }}">
 
                                 @if ($errors->has('title'))
@@ -34,8 +34,8 @@
                             </div>
                         </div>
 
-                        <div class="field {{ $errors->has('email') ? ' has-error' : '' }}">
-                            <div class="col-md-6">
+                        <div class=" {{ $errors->has('email') ? ' has-error' : '' }}">
+                            <div class="margin-bottom-large">
                                 <input type="email" class="form-control" placeholder="Email" name="email" value="{{ old('email') }}">
 
                                 @if ($errors->has('email'))
@@ -47,7 +47,7 @@
                         </div>
 
                         <div class="field {{ $errors->has('password') ? ' has-error' : '' }}">
-                            <div class="col-md-6">
+                            <div class="margin-bottom-large">
                                 <input type="password" class="form-control" placeholder="Password" name="password">
 
                                 @if ($errors->has('password'))
@@ -58,8 +58,8 @@
                             </div>
                         </div>
 
-                        <div class="field {{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                            <div class="col-md-6">
+                        <div class="{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                            <div class="margin-bottom-large">
                                 <input type="password" class="form-control" placeholder="Password confirmation" name="password_confirmation">
 
                                 @if ($errors->has('password_confirmation'))
@@ -70,8 +70,8 @@
                             </div>
                         </div>
 
-                        <div class="field">
-                            <div class="col-md-6 col-md-offset-4">
+                        <div class="">
+                            <div class="">
                                 <button type="submit" class="ui button large fluid green">
                                     <i class="fa fa-btn fa-user"></i>Register
                                 </button>
